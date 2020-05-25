c20cd65a089a57b645b23484ca9bfbc53ce2451d
pedro braz <braz.hpedro@gmail.com>
2020-05-25 20:47:14 -0300

    Adicionar arquivos de projeto.

---
+++ Drawing/Models/Trace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Drawing.Models
+{
+    public class Trace
+    {
+    public Point initial { get; set; }
+    public Point final { get; set; }
+    public string color { get; set; }
+    }
+
+    public class Point
+    {
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+    }
+}
